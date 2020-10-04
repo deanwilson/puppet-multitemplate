ab6d8d12fd5efc38d2e2e91d73ecd416c5ef5ccc
Dean Wilson <dean.wilson@gmail.com>
2020-10-04 23:39:20 +0100

    Fix 'RSpec/ImplicitExpect: Prefer is_expected.to over should.'

--- spec/functions/multitemplate_spec.rb
+++ spec/functions/multitemplate_spec.rb
@@ -5,13 +5,13 @@ describe 'multitemplate' do
 
   describe 'when a single template is specified' do
     it 'returns the contents of the given template' do
-      should run.with_params('single_match/hello.erb').and_return('Hello World')
+      is_expected.to run.with_params('single_match/hello.erb').and_return('Hello World')
     end
   end
 
   describe 'when multiple templates are specified' do
     it 'returns the contents of the first existing one' do
-      should run.with_params('single_match/nosuch.erb', 'single_match/hello.erb').and_return('Hello World')
+      is_expected.to run.with_params('single_match/nosuch.erb', 'single_match/hello.erb').and_return('Hello World')
     end
   end
 
@@ -19,15 +19,15 @@ describe 'multitemplate' do
 
   describe 'handling expected error conditions' do
     it 'throws Puppet::ParseError if called with no templates' do
-      should run.with_params.and_raise_error(Puppet::ParseError)
+      is_expected.to run.with_params.and_raise_error(Puppet::ParseError)
     end
 
     it 'throws Puppet::ParseError when given an invalid template' do
-      should run.with_params('invalid_template/invalid.erb').and_raise_error(Puppet::ParseError)
+      is_expected.to run.with_params('invalid_template/invalid.erb').and_raise_error(Puppet::ParseError)
     end
 
     it 'throws Puppet::ParseError if no given templates exist' do
-      should run.with_params('invalid_template/nosuch.erb').and_raise_error(Puppet::ParseError)
+      is_expected.to run.with_params('invalid_template/nosuch.erb').and_raise_error(Puppet::ParseError)
     end
   end
 end
